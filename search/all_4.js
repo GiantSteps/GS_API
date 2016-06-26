32c8c6f8e0b019d82665b402d78424f58484f1ac
Martin Hermant <martin.hermant@gmail.com>
2016-06-27 01:10:00 +0200

    updateCoreDoc

--- search/all_4.js
+++ search/all_4.js
@@ -4,6 +4,7 @@ var searchData=
   ['getallidenticalevents',['getAllIdenticalEvents',['../classpython_wrap_1_1_g_s_pattern_1_1_g_s_pattern_1_1_g_s_pattern.html#a6da0f1981132e51aeb0a1b174de8b95b',1,'pythonWrap::GSPattern::GSPattern::GSPattern']]],
   ['getalltags',['getAllTags',['../classpython_wrap_1_1_g_s_pattern_1_1_g_s_pattern_1_1_g_s_pattern.html#adf607ca8fc8d85d48e04eeb9faa2089d',1,'pythonWrap::GSPattern::GSPattern::GSPattern']]],
   ['getlastnoteoff',['getLastNoteOff',['../classpython_wrap_1_1_g_s_pattern_1_1_g_s_pattern_1_1_g_s_pattern.html#a69a86065c12a222349c1444f563b55de',1,'pythonWrap::GSPattern::GSPattern::GSPattern']]],
+  ['getpatternfortimeslice',['getPatternForTimeSlice',['../classpython_wrap_1_1_g_s_pattern_1_1_g_s_pattern_1_1_g_s_pattern.html#a5fa9d8970febf1b7f0bd975bb177dc55',1,'pythonWrap::GSPattern::GSPattern::GSPattern']]],
   ['getstartingeventsattime',['getStartingEventsAtTime',['../classpython_wrap_1_1_g_s_pattern_1_1_g_s_pattern_1_1_g_s_pattern.html#a2d4c5aa660f8eeac2fb3c8b942e276f0',1,'pythonWrap::GSPattern::GSPattern::GSPattern']]],
   ['gsmarkovstyle',['GSMarkovStyle',['../classpython_wrap_1_1_g_s_style_1_1_g_s_markov_style_1_1_g_s_markov_style.html',1,'pythonWrap::GSStyle::GSMarkovStyle']]],
   ['gspattern',['GSPattern',['../classpython_wrap_1_1_g_s_pattern_1_1_g_s_pattern_1_1_g_s_pattern.html',1,'pythonWrap::GSPattern::GSPattern']]],
