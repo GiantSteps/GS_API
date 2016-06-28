d8738302a9b32cf9a43d4f390487225fe84f62be
Martin Hermant <martin.hermant@gmail.com>
2016-06-28 08:44:51 +0200

    document style

--- pythonWrap/GSStyle/GSMarkovStyle.py
+++ pythonWrap/GSStyle/GSMarkovStyle.py
@@ -1,9 +1,21 @@
 from GSStyle import GSStyle
 import random
+import numpy as np
+from numpy.random import random_sample
 import copy
 
 class GSMarkovStyle(GSStyle):
+	""" compute sa style based on markov chains
 
+	Args:
+		order: order used for markov computation
+		numSteps: number of steps to consider (binarization of pattern)
+
+	Attributes:
+		order: order used for markov computation
+		numSteps: number of steps to consider (binarization of pattern)
+
+	"""
 	def __init__(self,order,numSteps):
 		super(GSStyle,self).__init__()
 		self.type = "None"
@@ -12,16 +24,21 @@ class GSMarkovStyle(GSStyle):
 
 
 	def generateStyle(self, PatternClasses):
+		""" generate style based on list of GSPattern
+		Args:
+			PatternClasses :  list of GSPatterns
+		"""
 		if not isinstance(PatternClasses,list):
 			print "style need a list"
 			return False
 		else:
 			self.originPatterns = PatternClasses;
-
 			self.buildStyle();
 
 
 	def buildStyle(self):
+		""" builds transisiont table for the previously given list of GSPattern
+		"""
 		self.transitionTable = [{} for f in range(self.numSteps)]
 
 		self.binarizedPatterns = copy.deepcopy(self.originPatterns)
@@ -42,35 +59,73 @@ class GSMarkovStyle(GSStyle):
 						curDic[combinationName][n] += 1
 				# print e.tags , [ x.tags  for t in lastEvents for x in t]
 		print self.transitionTable
+		def _normalize():
+			for t in self.transitionTable:
+				for d in t:
+					sum= 0
+					for pe in t[d]:
+						sum+= t[d][pe]
+					
+					for pe in t[d]:
+						t[d][pe]/=1.0*sum
+		_normalize()
+		print self.transitionTable
 
 	def generatePattern(self,seed = None):
+		""" generate a new pattern
+		Args:
+			seed: seed used for random initialisation of pattern (value of None generates a new one)
+		"""
 		random.seed(seed)
 		potentialStartEvent = []
 
-		def getAvailableAtStep(step):
+		def _getAvailableAtStep(step):
 			res = []
+			if step < 0 : step+=len(self.transitionTable)
 			for n in self.transitionTable[step]:
 				for t in self.transitionTable[step][n]:
 					res+=[t]
 			return res
 
-		def isValidState(step,previousTags):
+		def _isValidState(step,previousTags):
 			d = self.transitionTable[step]
 			return previousTags in d
 
-		
+		def _generateEventAtStep(step,previousTags):
+			if not previousTags in self.transitionTable[step] : return None
+			d = self.transitionTable[step][previousTags]
+			bins = np.cumsum([d[x] for x in d])
+			chosenIdx = np.digitize(random.random(), bins);
+			
+			return d.keys()[chosenIdx]
+			
+			
+			
+
+
 		
 		cIdx = self.order
 		startHypothesis =[]
 		for n in range (self.order):
-			startHypothesis+=[random.choice(getAvailableAtStep(n))]
-		while not isValidState(cIdx,','.join(startHypothesis)):
+			startHypothesis+=[random.choice(_getAvailableAtStep(n))]
+		while not _isValidState(cIdx,','.join(startHypothesis)):
 			startHypothesis =[]
 			for n in range (self.order):
-				startHypothesis+=[random.choice(getAvailableAtStep(n))]
-			
-		eventTags = startHypothesis
+				startHypothesis+=[random.choice(_getAvailableAtStep(n))]
+
 		print startHypothesis
+		events = startHypothesis
+		i = self.order
+		while i <self.numSteps:
+			print "ii"+str(i)
+			print events[i-self.order:i]
+			newEv =  _generateEventAtStep(i,','.join(events[i-self.order:i]))
+			if newEv:
+				events+=[newEv]
+				i+=1
+			
+
+
 
 		
 
--- pythonWrap/GSStyle/GSStyle.py
+++ pythonWrap/GSStyle/GSStyle.py
@@ -1,7 +1,15 @@
 from GSPattern import GSPattern
 
 class GSStyle(object):
-	
+	""" base class for defining a style
+
+	such class needs to provide following functions :
+		generateStyle(self, PatternClasses) : compute inner state of style based on list of patterns
+		getDistanceFromStyle(self,Pattern) : return a normalized value representing the "styliness" of a pattern 1 being farthest from style
+		getClosestPattern(self,Pattern,seed=0) : return the closest pattern in this style
+		getInterpolated(self,PatternA,PatternB,distanceFromA,seed=0) : interpolate between two pattern given this style constraints
+
+	"""
 	def __init__():
 		self.type = "None"
 
