c476d59f1bc7175dc7821e27e55b1cab32b73635
martin hermant <martin.hermant@gmail.com>
2016-11-11 11:51:51 +0100

    start version 1.0.1dev GSAPI add global help for gsapi module

--- python/gsapi/__init__.py
+++ python/gsapi/__init__.py
@@ -1,3 +1,18 @@
+"""
+The GS_API is a Python/C++ library for manipulating musical symbolic data.
+all modules are documented, for more infos on the given module 
+ type
+
+ >> help(gsapi.modulename)
+
+
+other useful resources :
+
+ * online tutorials and documentation : https://giantsteps.github.io/GS_API
+ * source code : https://github.com/GiantSteps/GS_API
+
+
+"""
 from GSLogging import gsapiLogger
 from GSPattern import GSPatternEvent, GSPattern
 from GSBassmineAnalysis import *
--- python/setup.py
+++ python/setup.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
 #                     sources = cppSources)
 
 setup(name='gsapi',
-      version=1.0,
+      version='1.0.1dev',
       description='Python symbolic music manipulation tools',
       long_description="",
       author='MTG / GiantSteps',
