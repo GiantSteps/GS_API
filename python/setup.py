d903e73c679022eaa4cfe8137552f6b8f9851c2f
Ángel Faraldo <angelfaraldo@gmail.com>
2016-12-06 08:26:13 +0100

    paralell

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,3 @@
-
 from setuptools import setup, find_packages
 from distutils.extension import Extension
 # from Cython.Distutils import build_ext
@@ -15,27 +14,21 @@ import glob
 # from gen import genBindings
 # genBindings.generate(bindingsFolder)
 
-
 import distutils.ccompiler
 import utils.parallelComp
-distutils.ccompiler.CCompiler.compile=utils.parallelComp.parallelCCompile
+
+distutils.ccompiler.CCompiler.compile = utils.parallelComp.parallelCCompile
 
 if __name__ == '__main__':
-      import sys
-      toAppend = ['clean','--all']
-      
-      toAppend = ['build']
-      toAppend = ['install']
-      # toAppend  = ['sdist','bdist_wheel','install']
-      if len(sys.argv)==1:
-            for s in toAppend:
-                  sys.argv.append(s)
-      print sys.argv
-
-
-
-
-
+    import sys
+    toAppend = ['clean', '--all']
+    toAppend = ['build']
+    toAppend = ['install']
+    # toAppend  = ['sdist','bdist_wheel','install']
+    if len(sys.argv) == 1:
+        for s in toAppend:
+            sys.argv.append(s)
+    print sys.argv
 
 # gsapiModule = Extension('gsapi',
 #                     # define_macros = [('MAJOR_VERSION', '1'),
@@ -44,27 +37,25 @@ if __name__ == '__main__':
 #                     libraries = [],
 #                     extra_compile_args=['-std=c++11','-w'],
 #                     library_dirs = ['/usr/local/lib'],
-                    
+
 #                     sources = cppSources)
 import gsapi
-
 setup(name='gsapi',
       version=gsapi.getGSAPIFullVersion(),
-      description='Python symbolic music manipulation tools',
+      description='Python Symbolic Music Manipulation Tools',
       long_description="",
-      author='MTG / GiantSteps',
-      author_email='',
-      url='https://github.com/Giantsteps',
+      author='Martin Hermant, Angel Faraldo, Pere Calopa',
+      author_email='angel.faraldo@upf.edu',
+      url='https://github.com/Giantsteps/gsapi',
       license='',
-      packages=find_packages(exclude=['utils','gen','test', 'docs']),
+      packages=find_packages(exclude=['utils', 'gen', 'test', 'docs']),
       # ext_modules=[gsapiModule],
       # package_data={'gsapi': package_data},
       exclude_package_data={'': ['tests', 'docs']},
       # scripts=scripts,
       # cmdclass={'build_ext': build_ext},
       test_suite='nose.collector',
-      install_requires = ['python-midi', 'midiutil'],
-      zip_safe = True
+      install_requires=['python-midi', 'midiutil'],
+      zip_safe=True
       # classifiers=classifiers
       )
-
